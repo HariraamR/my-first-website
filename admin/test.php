524eb318c6bd542fa8c171feee3372cbb3f18cc6
HariraamR <134614393+HariraamR@users.noreply.github.com>
2023-05-25 20:06:27 +0530

    Add files via upload

---
+++ admin/test.php
@@ -0,0 +1,10 @@
+<pre>
+<?php
+include '/home/Vignesh/htdocs/NFT/includer.php';
+?>
+<h1>login</h1>
+<form action="auth.php" method="POST">
+<input type="password" name="password" placeholder="passwd">
+<input style="align-items: center" type="submit" value="Submit">
+</form>
+</pre>
