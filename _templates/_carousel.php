27e6aded490e8e931e376d92d761697f105c3a69
HariraamR <134614393+HariraamR@users.noreply.github.com>
2023-05-25 19:59:44 +0530

    Create _carousel.php

---
+++ _templates/_carousel.php
@@ -0,0 +1,56 @@
+<div id="myCarousel" class="carousel slide"></div>
+<div class="carousel-inner">
+	<div class="carosel-item active">
+		<div class="container">
+			<h1>
+				This is heading type.
+			</h1>
+			<p>
+				this is subtitle for the heading.
+			</p>
+		</div>
+	</div>
+	<div class="carosel-item">
+		<div class="container">
+			<h1>
+				This is heading type.
+			</h1>
+			<p>
+				this is subtitle for the heading.
+			</p>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<style>
+	/*
+	.carosel-item active {
+		height: 32rem;
+		background: black;
+		color: white;
+		position: relative;
+
+	}
+
+	.container {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		padding-bottom: 50px;
+
+	}*/
+</style>
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+<link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
+	integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+	integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous">
+</script>
