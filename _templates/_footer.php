76dbe0ddeef716b99d0fb06839b40b5b93bc78ae
HariraamR <134614393+HariraamR@users.noreply.github.com>
2023-05-25 21:33:50 +0530

    Update _footer.php

--- _templates/_footer.php
+++ _templates/_footer.php
@@ -111,7 +111,7 @@
     		<div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
     			<p>&copy; 2022 Company, Inc. All rights reserved.</p>
     			<ul class="list-unstyled d-flex">
-    				<li class="ms-3"><a class="link-dark" target="_blank" href="https://twitter.com/sbvignesh"><svg
+    				<li class="ms-3"><a class="link-dark" target="_blank" href="https://twitter.com/"><svg
     							class="bi" width="24" height="24">
     							<use xlink:href="#twitter" />
     						</svg></a></li>
